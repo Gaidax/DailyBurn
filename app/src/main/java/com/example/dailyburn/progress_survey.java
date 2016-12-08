c92f2c154afb188fba9706a7731f5db462dde4ce
Rukshar Patel <rukhsarpatel555@gmail.com>
2016-12-07 19:01:22 -0500

    Client database tables - (Feedback & Reminder)

---
+++ app/src/main/java/com/example/dailyburn/progress_survey.java
@@ -0,0 +1,13 @@
+package com.example.dailyburn;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class progress_survey extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_progress_survey);
+    }
+}
