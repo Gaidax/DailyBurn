a6cebb11416de717d49e5eedfabace0f330ebe89
Rukshar Patel <rukhsarpatel555@gmail.com>
2016-12-16 18:35:12 -0500

    Added Tutorial Video

--- app/src/main/java/com/example/dailyburn/AppTutorialVideo.java
+++ app/src/main/java/com/example/dailyburn/AppTutorialVideo.java
@@ -1,13 +1,43 @@
 package com.example.dailyburn;
 
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.PixelFormat;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.VideoView;
 
 public class AppTutorialVideo extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_app_tutorial_video);
+
+        Button buttonPlayVideo2 = (Button)findViewById(R.id.btnPlay);
+        getWindow().setFormat(PixelFormat.UNKNOWN);
+//displays a video file
+        VideoView mVideoView2 = (VideoView)findViewById(R.id.videoView);
+        String uriPath2 = "android.resource://com.example.dailyburn/"+R.raw.output;
+        Uri uri2 = Uri.parse(uriPath2);
+        mVideoView2.setVideoURI(uri2);
+        mVideoView2.requestFocus();
+        mVideoView2.start();
+        buttonPlayVideo2.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                VideoView mVideoView2 = (VideoView) findViewById(R.id.videoView);
+
+                String uriPath = "android.resource://com.example.dailyburn/" + R.raw.output;
+                Uri uri2 = Uri.parse(uriPath);
+                mVideoView2.setVideoURI(uri2);
+                mVideoView2.requestFocus();
+                mVideoView2.start();
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/dailyburn/Summary.java
+++ app/src/main/java/com/example/dailyburn/Summary.java
@@ -1,5 +1,6 @@
 package com.example.dailyburn;
 
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 /**
@@ -9,7 +10,10 @@ import android.support.v7.app.AppCompatActivity;
 public class Summary extends AppCompatActivity{
 
 
-
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_summary);
+    }
 
 
 }
